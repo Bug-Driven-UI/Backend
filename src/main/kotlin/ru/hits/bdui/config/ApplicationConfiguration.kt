41656f420e7e7f34a3de92a54213c0b8094b4dca
Васильев Данил <danil.a.vasilev@gazprombank.ru>
2025-09-30 20:44:18 +0700

    Из ответов убраны поля равные null

--- src/main/kotlin/ru/hits/bdui/config/ApplicationConfiguration.kt
+++ src/main/kotlin/ru/hits/bdui/config/ApplicationConfiguration.kt
@@ -1,10 +1,12 @@
 package ru.hits.bdui.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.web.reactive.function.client.WebClient
 import ru.hits.bdui.engine.Interpreter
 import ru.hits.bdui.engine.expression.JSInterpreter
@@ -12,8 +14,10 @@ import ru.hits.bdui.engine.expression.JSInterpreter
 @Configuration
 class ApplicationConfiguration {
     @Bean("CustomObjectMapper")
+    @Primary
     fun customObjectMapper(): ObjectMapper =
         jacksonObjectMapper()
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
     @Bean("CustomWebClient")
