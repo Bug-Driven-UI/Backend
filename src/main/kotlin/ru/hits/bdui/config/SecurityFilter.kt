049142b28ead931be7d5424f9db62d25f20b064f
Васильев Данил <danil.a.vasilev@gazprombank.ru>
2025-10-17 17:48:24 +0700

    Добавлена легендарная проверка токена

--- src/main/kotlin/ru/hits/bdui/config/SecurityFilter.kt
+++ src/main/kotlin/ru/hits/bdui/config/SecurityFilter.kt
@@ -1,5 +1,6 @@
 package ru.hits.bdui.config
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.WebFilter
 
 @Configuration
@@ -18,7 +22,8 @@ class SecurityConfiguration {
         WebFilter { exchange, chain ->
             val authHeader = exchange.request.headers.getFirst(HttpHeaders.AUTHORIZATION)
 
-            if (authHeader != securityProperties.apiKey) {
+            //TODO(УДАЛИТЬ ЭТУ ЛЕГЕНДАРНУЮ ЗАЩИТУ)
+            if (!exchange.request.path.value().endsWith("/check") && authHeader != securityProperties.apiKey) {
                 exchange.response.statusCode = HttpStatus.UNAUTHORIZED
                 exchange.response.setComplete()
             } else {
@@ -31,4 +36,14 @@ class SecurityConfiguration {
 data class SecurityProperties(
     val apiKey: String,
 )
+
+@RestController
+@ConditionalOnBean(SecurityConfiguration::class)
+class SecurityController(
+    private val securityProperties: SecurityProperties
+) {
+    @GetMapping("/check")
+    fun check(@RequestHeader(name = HttpHeaders.AUTHORIZATION) apiKey: String): Boolean =
+        securityProperties.apiKey == apiKey
+}
 
