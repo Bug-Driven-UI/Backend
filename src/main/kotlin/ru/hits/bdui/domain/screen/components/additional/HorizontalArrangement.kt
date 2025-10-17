6551367c4dd22c6cb022161d64d7e5101903f3e6
Васильев Данил <danil.a.vasilev@gazprombank.ru>
2025-10-17 14:06:06 +0700

    Внесена правка

--- src/main/kotlin/ru/hits/bdui/domain/screen/components/additional/HorizontalArrangement.kt
+++ src/main/kotlin/ru/hits/bdui/domain/screen/components/additional/HorizontalArrangement.kt
@@ -4,29 +4,10 @@ package ru.hits.bdui.domain.screen.components.additional
  * Отвечает за расположение детей в Row
  */
 sealed interface HorizontalArrangement {
-    val type: String
-
-    data object Start : HorizontalArrangement {
-        override val type: String = "start"
-    }
-
-    data object End : HorizontalArrangement {
-        override val type: String = "end"
-    }
-
-    data object Center : HorizontalArrangement {
-        override val type: String = "center"
-    }
-
-    data object SpaceBetween : HorizontalArrangement {
-        override val type: String = "spaceBetween"
-    }
-
-    data object SpaceEvenly : HorizontalArrangement {
-        override val type: String = "spaceEvenly"
-    }
-
-    data object SpaceAround : HorizontalArrangement {
-        override val type: String = "spaceAround"
-    }
+    data object Start : HorizontalArrangement
+    data object End : HorizontalArrangement
+    data object Center : HorizontalArrangement
+    data object SpaceBetween : HorizontalArrangement
+    data object SpaceEvenly : HorizontalArrangement
+    data object SpaceAround : HorizontalArrangement
 }
